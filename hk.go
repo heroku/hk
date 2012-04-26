ba03cda5ddc753e64371e58199b218510e8ec659
Mark McGranaghan <mmcgrana@gmail.com>
2012-04-26 12:29:07 -0700

    rearange

--- hk.go
+++ hk.go
@@ -44,64 +44,6 @@ func unrecCmd(cmd string) {
 	error(fmt.Sprintf("'%s' is not an hk command. See 'hk help'", cmd))
 }
 
-func usage() {
-	fmt.Printf("Usage: hk <command> [-a <app>] [command-specific-options]\n\n")
-	fmt.Printf("Supported hk commands are:\n")
-	fmt.Printf("  addons          List add-ons\n")
-	fmt.Printf("  addons-add      Add an add-on\n")
-	fmt.Printf("  addons-open     Open an add-on page\n")
-	fmt.Printf("  addons-remove   Remove an add-on \n")
-	fmt.Printf("  create          Create an app\n")
-	fmt.Printf("  destroy         Destroy an app\n")
-	fmt.Printf("  env             List config vars\n")
-	fmt.Printf("  get             Get config var\n")
-	fmt.Printf("  help            Show this help\n")
-	fmt.Printf("  info            Show app info\n")
-	fmt.Printf("  list            List apps\n")
-	fmt.Printf("  login           Log in\n")
-	fmt.Printf("  logout          Log out\n")
-	fmt.Printf("  logs            Show logs\n")
-	fmt.Printf("  open            Open app\n")
-	fmt.Printf("  pg              List databases\n")
-	fmt.Printf("  pg-info         Show database info\n")
-	fmt.Printf("  pg-promote      Promote a database\n")
-	fmt.Printf("  ps-psql         Open a psql database shell\n")
-	fmt.Printf("  pg-wait         Await a database\n")
-	fmt.Printf("  ps              List processes\n")
-	fmt.Printf("  release         Show release info\n")
-	fmt.Printf("  releases        List releases\n")
-	fmt.Printf("  rename          Rename an app\n")
-	fmt.Printf("  restart         Restart processes\n")
-	fmt.Printf("  rollback        Rollback to a previous release\n")
-	fmt.Printf("  run             Run a process\n")
-	fmt.Printf("  set             Set config var\n")
-	fmt.Printf("  scale           Scale processes\n")
-	fmt.Printf("  run             Run a process\n")
-	fmt.Printf("  stop            Stop a process\n")
-	fmt.Printf("  token           Show auth token\n")
-	fmt.Printf("  unset           Unset config vars\n")
-	fmt.Printf("  version         Display version\n\n")
-	fmt.Printf("See 'hk help <command>' for more information on a specific command.\n")
-	os.Exit(0)
-}
-
-func help() {
-	if len(os.Args) <= 2 {
-		usage()
-	} else {
-		cmd := os.Args[2]
-	  if cmd == "get" {
-			getHelp()
-		} else if cmd == "list" {
-		  listHelp()
-		} else if cmd == "version" {
-			versionHelp()
-		} else {
-			unrecCmd(cmd)
-		}
-	}
-}
-
 func getHelp() {
 	fmt.Printf("Usage: hk get -a <app> <key>\n\n")
 	fmt.Printf("Get the value of a config var.\n")
@@ -173,6 +115,64 @@ func version() {
   }
 	fmt.Printf("%s\n", VERSION)
 	os.Exit(0)
+}
+
+func usage() {
+	fmt.Printf("Usage: hk <command> [-a <app>] [command-specific-options]\n\n")
+	fmt.Printf("Supported hk commands are:\n")
+	fmt.Printf("  addons          List add-ons\n")
+	fmt.Printf("  addons-add      Add an add-on\n")
+	fmt.Printf("  addons-open     Open an add-on page\n")
+	fmt.Printf("  addons-remove   Remove an add-on \n")
+	fmt.Printf("  create          Create an app\n")
+	fmt.Printf("  destroy         Destroy an app\n")
+	fmt.Printf("  env             List config vars\n")
+	fmt.Printf("  get             Get config var\n")
+	fmt.Printf("  help            Show this help\n")
+	fmt.Printf("  info            Show app info\n")
+	fmt.Printf("  list            List apps\n")
+	fmt.Printf("  login           Log in\n")
+	fmt.Printf("  logout          Log out\n")
+	fmt.Printf("  logs            Show logs\n")
+	fmt.Printf("  open            Open app\n")
+	fmt.Printf("  pg              List databases\n")
+	fmt.Printf("  pg-info         Show database info\n")
+	fmt.Printf("  pg-promote      Promote a database\n")
+	fmt.Printf("  ps-psql         Open a psql database shell\n")
+	fmt.Printf("  pg-wait         Await a database\n")
+	fmt.Printf("  ps              List processes\n")
+	fmt.Printf("  release         Show release info\n")
+	fmt.Printf("  releases        List releases\n")
+	fmt.Printf("  rename          Rename an app\n")
+	fmt.Printf("  restart         Restart processes\n")
+	fmt.Printf("  rollback        Rollback to a previous release\n")
+	fmt.Printf("  run             Run a process\n")
+	fmt.Printf("  set             Set config var\n")
+	fmt.Printf("  scale           Scale processes\n")
+	fmt.Printf("  run             Run a process\n")
+	fmt.Printf("  stop            Stop a process\n")
+	fmt.Printf("  token           Show auth token\n")
+	fmt.Printf("  unset           Unset config vars\n")
+	fmt.Printf("  version         Display version\n\n")
+	fmt.Printf("See 'hk help <command>' for more information on a specific command.\n")
+	os.Exit(0)
+}
+
+func help() {
+	if len(os.Args) <= 2 {
+		usage()
+	} else {
+		cmd := os.Args[2]
+	  if cmd == "get" {
+			getHelp()
+		} else if cmd == "list" {
+		  listHelp()
+		} else if cmd == "version" {
+			versionHelp()
+		} else {
+			unrecCmd(cmd)
+		}
+	}
 }
 
 func main() {
