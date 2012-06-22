aa35764f8376dd556bd08d940bb831e4158cf734
Lincoln Stoll <lstoll@lstoll.net>
2012-06-22 10:22:04 +0200

    Additional logplex options for tail

--- tail.go
+++ tail.go
@@ -7,20 +7,29 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 )
 
 var cmdTail = &Command{
 	Run:   runTail,
-	Usage: "tail [-a APP] [-f]",
+	Usage: "tail [-a APP] [-f] [-n LINES] [-s SOURCE] [-p PROCESS]",
 	Short: "tail log files",
 	Long:  `Tail tails log files.`,
 }
 
-var follow bool
+var (
+	follow  bool
+	lines   int
+	source  string
+	process string
+)
 
 func init() {
 	cmdTail.Flag.StringVar(&flagApp, "a", "", "app")
 	cmdTail.Flag.BoolVar(&follow, "f", false, "do not stop when end of file is reached")
+	cmdTail.Flag.IntVar(&lines, "n", -1, "number of log lines to request")
+	cmdTail.Flag.StringVar(&source, "s", "", "only display logs from the given source")
+	cmdTail.Flag.StringVar(&process, "p", "", "only display logs from the given process")
 }
 
 func runTail(cmd *Command, args []string) {
@@ -29,6 +38,18 @@ func runTail(cmd *Command, args []string) {
 
 	if follow {
 		data.Add("tail", "1")
+	}
+
+	if lines > 0 {
+		data.Add("num", strconv.Itoa(lines))
+	}
+
+	if source != "" {
+		data.Add("source", source)
+	}
+
+	if process != "" {
+		data.Add("ps", process)
 	}
 
 	req := APIReq("GET", "/apps/"+mustApp()+"/logs")
