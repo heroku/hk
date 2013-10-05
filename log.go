5749935955ceff175a0ed637dd5103bf6694bc46
Troels Thomsen <troels@thomsen.io>
2013-10-05 13:06:47 +0200

    Define method for resolving a color

--- log.go
+++ log.go
@@ -141,3 +141,13 @@ func newColorizer(writer LineWriter) *colorizer {
 		writer: writer,
 	}
 }
+
+func (c *colorizer) resolve(p string) string {
+	if color, ok := c.colors[p]; ok {
+		return color
+	}
+
+	color := c.colorScheme[len(c.colors)%len(c.colorScheme)]
+	c.colors[p] = color
+	return color
+}
