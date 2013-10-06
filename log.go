6f0a59bb31aa4ffad7d1ded0a51f909a3a049cb4
Troels Thomsen <troels@thomsen.io>
2013-10-06 11:48:22 +0200

    Define a writer adapter

--- log.go
+++ log.go
@@ -110,3 +110,11 @@ func runLog(cmd *Command, args []string) {
 type LineWriter interface {
 	Writeln(p string) (int, error)
 }
+
+type WriterAdapter struct {
+	io.Writer
+}
+
+func (w WriterAdapter) Writeln(p string) (n int, err error) {
+	return fmt.Fprintln(w, p)
+}
