2f75262351e52530904569448b312c451e24fe59
Blake Gentry <blakesgentry@gmail.com>
2013-10-16 16:09:05 -0700

    Streamline the destroy command
    Don't care whether the app has addons or dynos. I told you to destroy
    it, so just f*cking destroy it :)

--- destroy.go
+++ destroy.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/exec"
 )
@@ -27,25 +26,8 @@ func runDestroy(cmd *Command, args []string) {
 		os.Exit(2)
 	}
 	name := args[0]
-	var dynos []*Dyno
-	err := Get(&dynos, "/apps/"+name+"/dynos")
-	if err == nil && len(dynos) > 0 {
-		log.Println("App", name, "has", len(dynos), "running dynos")
-		log.Println("To destroy this app, first scale to zero")
-		log.Println("To list dynos: hk ps")
-		log.Println("For more on scaling: hk help scale")
-		os.Exit(1)
-	}
-	if res := getMergedAddons(name); len(res) > 0 {
-		log.Println("App", name, "has", len(res), "addons")
-		log.Println("To destroy this app, first remove its addons")
-		log.Println("To list them: hk ls addons")
-		// TODO(kr): uncomment this when the command is known
-		//log.Println("For more on removing addons: hk help XXXXX")
-		os.Exit(1)
-	}
-	must(Delete("/apps/" + args[0]))
-	for _, remote := range gitRemotes(gitURL(args[0])) {
+	must(Delete("/apps/" + name))
+	for _, remote := range gitRemotes(gitURL(name)) {
 		exec.Command("git", "remote", "rm", remote).Run()
 	}
 }
