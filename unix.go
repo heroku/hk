e573071038533d2dde3743d219979d88fc32521a
mattn <mattn.jp@gmail.com>
2013-10-23 20:43:16 +0900

    sysExec on windows

--- unix.go
+++ unix.go
@@ -4,8 +4,13 @@ package main
 
 import (
 	"os"
+	"syscall"
 )
 
 var (
 	homePath = os.Getenv("HOME")
 )
+
+func sysExec(path string, args []string, env []string) error {
+	return syscall.Exec(path, args, env)
+}
