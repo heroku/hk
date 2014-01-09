133a6b652dbbec3b9c136a6759c720e5f83fbd65
Blake Gentry <blakesgentry@gmail.com>
2014-01-09 14:59:32 -0800

    move some stuff out into util.go

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -16,7 +15,6 @@ import (
 	"runtime"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/bgentry/go-netrc/netrc"
 	"github.com/bgentry/heroku-go"
@@ -295,18 +293,6 @@ func getCreds(u string) (user, pass string) {
 	return m.Login, m.Password
 }
 
-// exists returns whether the given file or directory exists or not
-func fileExists(path string) (bool, error) {
-	_, err := os.Stat(path)
-	if err == nil {
-		return true, nil
-	}
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-	return false, err
-}
-
 func app() (string, error) {
 	if flagApp != "" {
 		return flagApp, nil
@@ -367,73 +353,4 @@ func mustApp() string {
 		log.Fatal(err)
 	}
 	return name
-}
-
-func must(err error) {
-	if err != nil {
-		printError("error:", err.Error())
-	}
-}
-
-func printError(prefix, message string) {
-	log.Fatal(colorizeMessage("red", prefix, message))
-}
-
-func colorizeMessage(color, prefix, message string) string {
-	prefResult := ""
-	if prefix != "" {
-		prefResult = ansi.Color(prefix, color+"+b") + " " + ansi.ColorCode("reset")
-	}
-	return prefResult + ansi.Color(message, color) + ansi.ColorCode("reset")
-}
-
-func listRec(w io.Writer, a ...interface{}) {
-	for i, x := range a {
-		fmt.Fprint(w, x)
-		if i+1 < len(a) {
-			w.Write([]byte{'\t'})
-		} else {
-			w.Write([]byte{'\n'})
-		}
-	}
-}
-
-type prettyTime struct {
-	time.Time
-}
-
-func (s prettyTime) String() string {
-	if time.Now().Sub(s.Time) < 12*30*24*time.Hour {
-		return s.Local().Format("Jan _2 15:04")
-	}
-	return s.Local().Format("Jan _2  2006")
-}
-
-func openURL(url string) error {
-	var command string
-	var args []string
-	switch runtime.GOOS {
-	case "darwin":
-		command = "open"
-		args = []string{command, url}
-	case "windows":
-		command = "cmd"
-		args = []string{"/c", "start " + url}
-	default:
-		if _, err := exec.LookPath("xdg-open"); err != nil {
-			log.Println("xdg-open is required to open web pages on " + runtime.GOOS)
-			os.Exit(2)
-		}
-		command = "xdg-open"
-		args = []string{command, url}
-	}
-	if runtime.GOOS != "windows" {
-		p, err := exec.LookPath(command)
-		if err != nil {
-			log.Printf("Error finding path to %q: %s\n", command, err)
-			os.Exit(2)
-		}
-		command = p
-	}
-	return sysExec(command, args, os.Environ())
 }
