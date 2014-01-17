c6b36fc14418a63b4bad6851401665097fbe4c13
Blake Gentry <blakesgentry@gmail.com>
2014-01-17 13:46:31 -0800

    show addons instead of URLs for those drains

--- log_drains.go
+++ log_drains.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"text/tabwriter"
@@ -13,25 +14,94 @@ var cmdLogDrains = &Command{
 	Category: "app",
 	Short:    "list log drains" + extra,
 	Long: `
-Lists log drains on an app.
+Lists log drains on an app. Shows the drain's ID, as well as its
+Add-on name (if it's from an Add-on) or its URL.
 
 Example:
 
     $ hk log-drains
-    7f89b6bb-08af-4343-b0b4-d0415dd81712  d.b5f66703-6fb2-4195-a4b1-3ab2f1e3423f  syslog://my.log.host
-    23fcdb8a-3095-46f5-abc2-c5f293c54cf1  d.3f17356d-3b5d-4de2-a6aa-cc367e4a8fc8  syslog://my.other.log.host
+    6af8b744-c513-4217-9f7c-1234567890ab  logging-addon:jumbo
+    7f89b6bb-08af-4343-b0b4-d0415dd81712  syslog://my.log.host
+    23fcdb8a-3095-46f5-abc2-c5f293c54cf1  syslog://my.other.log.host
 `,
 }
 
 func runLogDrains(cmd *Command, args []string) {
-	drains, err := client.LogDrainList(mustApp(), nil)
+	if len(args) != 0 {
+		cmd.printUsage()
+		os.Exit(2)
+	}
+	appname := mustApp()
+
+	// fetch app's addons concurrently in case we need to resolve addon names
+	addonsch := make(chan []heroku.Addon, 1)
+	errch := make(chan error, 1)
+	go func(appname string) {
+		if addons, err := client.AddonList(appname, nil); err != nil {
+			errch <- err
+		} else {
+			addonsch <- addons
+		}
+	}(appname)
+
+	drains, err := client.LogDrainList(appname, nil)
 	must(err)
+
+	hasAddonDrains := false
+	merged := make([]*mergedLogDrain, len(drains))
+	for i := range drains {
+		if !hasAddonDrains && drains[i].Addon != nil {
+			hasAddonDrains = true
+		}
+		merged[i] = &mergedLogDrain{drain: drains[i], hasAddon: drains[i].Addon != nil}
+	}
+
+	if hasAddonDrains {
+		// resolve addon names, use those instead of URLs
+		select {
+		case _ = <-errch:
+			// couldn't resolve addons, just move on
+		case addons := <-addonsch:
+			mergeDrainAddonInfo(merged, addons)
+		}
+	}
 
 	w := tabwriter.NewWriter(os.Stdout, 1, 2, 2, ' ', 0)
 	defer w.Flush()
 
-	for _, drain := range drains {
-		listRec(w, drain.Id, drain.Token, drain.URL)
+	for _, m := range merged {
+		listRec(w, m.drain.Id, m.addonNameOrURL())
+	}
+}
+
+type mergedLogDrain struct {
+	drain    heroku.LogDrain
+	hasAddon bool
+	addon    *heroku.Addon
+}
+
+func (m *mergedLogDrain) addonNameOrURL() string {
+	switch {
+	case m.hasAddon && m.addon != nil:
+		return m.addon.Plan.Name
+	case m.hasAddon:
+		return "unknown"
+	default:
+		return m.drain.URL
+	}
+}
+
+// merge addon info into log drains
+func mergeDrainAddonInfo(merged []*mergedLogDrain, addons []heroku.Addon) {
+	for i := range merged {
+		if merged[i].hasAddon {
+			for j := range addons {
+				if merged[i].drain.Addon.Id == addons[j].Id {
+					merged[i].addon = &addons[j]
+					break
+				}
+			}
+		}
 	}
 }
 
