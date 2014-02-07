6406be83819d5429024465e1b65d711241718be5
Blake Gentry <blakesgentry@gmail.com>
2014-02-07 12:08:18 -0800

    prompt for confirmation on destroy
    Accepts confirmation via pipe to stdin, or prompts the user to confirm.
    
    Fixes #90.

--- destroy.go
+++ destroy.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+
+	"github.com/heroku/hk/term"
+	"github.com/mgutz/ansi"
 )
 
 var cmdDestroy = &Command{
@@ -12,13 +16,17 @@ var cmdDestroy = &Command{
 	Category: "app",
 	Short:    "destroy an app",
 	Long: `
-Destroy destroys a heroku app.
-
-There is no going back, so be sure you mean it.
+Destroy destroys a heroku app. There is no going back, so be
+sure you mean it. The command will prompt for confirmation, or
+accept confirmation via stdin.
 
 Example:
 
     $ hk destroy myapp
+    warning: This will destroy myapp and its add-ons. Please type "myapp" to continue:
+    Destroyed myapp.
+
+    $ echo myapp | hk destroy myapp
     Destroyed myapp.
 `,
 }
@@ -29,6 +37,20 @@ func runDestroy(cmd *Command, args []string) {
 		os.Exit(2)
 	}
 	appname := args[0]
+
+	if term.IsTerminal(os.Stdin) {
+		printWarning("This will destroy %s and its add-ons. Please type %q to continue:", appname, appname)
+		fmt.Printf(ansi.Color("> ", "+b") + ansi.ColorCode("reset"))
+	}
+	var confirm string
+	if _, err := fmt.Scanln(&confirm); err != nil {
+		printFatal(err.Error())
+	}
+
+	if confirm != appname {
+		printFatal("Confirmation did not match %q.", appname)
+	}
+
 	must(client.AppDelete(appname))
 	log.Printf("Destroyed %s.", appname)
 	remotes, _ := gitRemotes()
