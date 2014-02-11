f7ec968b70f3d2f64fa0f9058be5504d272df1e5
Blake Gentry <blakesgentry@gmail.com>
2014-02-11 13:44:26 -0800

    tests for stringsIndex()

--- util_test.go
+++ util_test.go
@@ -49,3 +49,24 @@ func TestNetrcPath(t *testing.T) {
 	}
 	os.Setenv("NETRC_PATH", "")
 }
+
+func TestStringsIndex(t *testing.T) {
+	a1 := []string{}
+	if res := stringsIndex(a1, ""); res != -1 {
+		t.Errorf("expected -1, got %d", res)
+	}
+	if res := stringsIndex(a1, "-a"); res != -1 {
+		t.Errorf("expected -1, got %d", res)
+	}
+
+	a2 := []string{"-a", "bbq"}
+	if res := stringsIndex(a2, "-a"); res != 0 {
+		t.Errorf("expected 0, got %d", res)
+	}
+	if res := stringsIndex(a2, "bbq"); res != 1 {
+		t.Errorf("expected 1, got %d", res)
+	}
+	if res := stringsIndex(a2, ""); res != -1 {
+		t.Errorf("expected -1, got %d", res)
+	}
+}
