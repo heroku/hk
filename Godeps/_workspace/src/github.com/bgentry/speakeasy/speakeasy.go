cdfc91d39ae43c783f9fe339c4b086c14b7c74a2
Blake Gentry <blakesgentry@gmail.com>
2014-04-28 15:20:43 -0700

    update speakeasy again to avoid reading extra input accidentally

--- Godeps/_workspace/src/github.com/bgentry/speakeasy/speakeasy.go
+++ Godeps/_workspace/src/github.com/bgentry/speakeasy/speakeasy.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ask the user to enter a password with input hidden. prompt is a string to
@@ -17,9 +18,22 @@ func Ask(prompt string) (password string, err error) {
 }
 
 func readline() (value string, err error) {
-	s := bufio.NewScanner(os.Stdin)
-	s.Scan()
+	var pw []byte
+	b := make([]byte, 1)
+	stdin := bufio.NewReader(os.Stdin)
+	for {
+		// read one byte at a time so we don't accidentally read extra bytes
+		_, err = stdin.Read(b)
+		if err != nil {
+			return
+		}
+		if b[0] == '\n' {
+			break
+		}
+		pw = append(pw, b[0])
+	}
+
 	// Carriage return after the user input.
 	fmt.Println("")
-	return s.Text(), s.Err()
+	return strings.TrimSuffix(string(pw), "\r"), nil
 }
